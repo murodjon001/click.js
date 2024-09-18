1324f6111a8abc3b8c2e0a0020645210236ea900
murodjon001 <shermuhamedovmurodjon@gmail.com>
2024-09-18 10:14:25 +0500

    fix: minor

--- dist/index.d.mts
+++ dist/index.d.mts
@@ -1,72 +1,167 @@
-interface ICompleteApiParams {
-    merchant_prepare_id: number
-    
+interface IMerchantApi{
+    setConnectionKeys(params: IConnectionKeysParams): void
+
+    createInvoice(params: ICreateInvoiceParams): Promise<ICreateInvoiceResponse>
+
+    checkInvoiceStatus(params: ICheckInvoiceStatusParams): Promise<ICheckInvoiceStatusResponse>
+    checkPaymentStatusByMerchantTransId(params: ICheckPaymentStatusByMerchantTransId): Promise<ICheckPaymentStatusByMerchantTransIdResponse>
+    checkPaymentStatus(params: ICheckPaymentStatusParams): Promise<ICheckPaymentStatusResponse>
+
+    refundPartial(params: IRefoundPartialParams): Promise<IRefoundPartialResponse>
+    cancellationPayment(params: ICancellationPaymentParams): Promise<ICancellationPaymentResponse>
+
+    createTokenCard(params: ICreateTokenCardParams): Promise<ICreateTokenCardResponse>
+    verificationTokenCard(params: IVerificationTokenCardParams): Promise<IVerificationTokenCardResponse>
+
+    payWithToken(params: IPayWithTokenParams): Promise<IPayWithTokenResponse>
+    deleteTokenCard(params: IDeleteTokenCardParams): Promise<IDeleteTokenCardResponse>
 }
 
-declare class ICompleteApiResponse {
-    click_trans_id: number
-    merchant_trans_id: string
-    merchant_confirm_id: number
-    error: number
+interface IDeleteTokenCardResponse {
+    error_code: number
     error_note: string
 }
 
-interface IPrepareApiParams {
-    click_trans_id: number 
-    click_paydoc_id: number
+interface IDeleteTokenCardParams{
+    service_id: number
+    card_token: number
+}
+
+interface IPayWithTokenResponse {
+    error_code: number
+    error_note: string
+    payment_id: number
+    payment_status: number
+}
+
+interface IPayWithTokenParams{
+    card_token: string
+    amount: number
+    merchant_trans_id: string
+}
+
+interface IVerificationTokenCardResponse {
+    error_code: number
+    error_note: string
+    card_number: string
+}
+
+interface IVerificationTokenCardParams{
+    service_id: number
+    card_token: string
+    sms_code: number
+}
+
+interface ICreateTokenCardResponse{
+    error_code: number
+    error_note: string
+    card_token: string
+    phone_number: string
+    temporary: boolean
+}
+
+interface ICreateTokenCardParams{
+    service_id: number
+    card_number: string
+    expire_date: string
+    temporary: boolean
+}
+
+interface ICreateInvoiceParams{
+    service_id: number
+    amount: number
+    phone_number: string
+    merchant_trans_id: string //requestId
+}
+
+interface ICreateInvoiceResponse{
+    error_code: number
+    error_note: string
+    invoice_id: number
+}
+
+interface ICheckInvoiceStatusParams {
+    service_id: number
+    invoice_id: number
+}
+
+interface ICheckInvoiceStatusResponse{
+    error_code: number
+    error_note: string
+    invoice_status: number
+    invoice_status_note: number
+}
+
+interface ICheckPaymentStatusParams{
+    payment_id: number
+    service_id: number
+}
+
+interface ICheckPaymentStatusResponse{
+    payment_id: number
+    error_code: number
+    error_note: string
+    payment_status: number
+}
+
+interface ICheckPaymentStatusByMerchantTransId {
+    service_id: number
+    merchant_trans_id: string
+    YYYY_MM_DD: string
+}
+
+interface ICheckPaymentStatusByMerchantTransIdResponse {
+    payment_id: number
+    error_code: number
+    error_note: string
+    payment_status: number
+}
+
+interface ICancellationPaymentParams{
+    service_id: number
+    payment_id: number
+}
+
+interface ICancellationPaymentResponse {
+    payment_id: number
+    error_code: number
+    error_note: string
+}
+
+interface IRefoundPartialParams {
+    payment_id: number
+    service_id: number
     amount: number
 }
 
-interface IPrepareApiResponse {
-    click_trans_id: number
-    merchant_trans_id: string
-    merchant_prepare_id: number
-    error: number
+interface IRefoundPartialResponse {
+    error_code: number
     error_note: string
 }
 
-interface IConnectionKeysParams {
-    merchant_trans_id: string;
-    service_id: string;
-    user_id: string;
+interface IConnectionKeysParams$1 {
+    merchant_id: number;
+    service_id: number;
+    user_id: number;
     secret_key: string
 }
 
-interface IShoppingApi {
-    setConnectionKeys(params: IConnectionKeysParams): void
-    prepare(params: IPrepareApiParams): Promise<IPrepareApiResponse> 
-    complete(params: ICompleteApiParams): Promise<ICompleteApiResponse>
-}
-
-declare class ShoppingApi implements IShoppingApi {
-    private clickApiUrl;
-    private merchant_trans_id;
+declare class MerchantApi implements IMerchantApi {
+    private secret_key;
+    private merchant_id;
     private service_id;
     private user_id;
-    private secret_key;
-    private click_trans_id;
-    private click_paydoc_id;
-    private amount;
-    private action;
-    private error;
-    private error_note;
-    private sign_time;
-    private sign_string;
-    setConnectionKeys(params: IConnectionKeysParams): void;
-    prepare(params: IPrepareApiParams): Promise<IPrepareApiResponse>;
-    complete(params: ICompleteApiParams): Promise<ICompleteApiResponse>;
-    /**
-     * Sets amount, click_trans_id and click_paydoc_id properties
-     * @private
-     * @param {IPrepareApiParams}
-     */
-    private set otherProperties(value);
-    /**
-     * Sets sign_string property
-     * @private
-     * @param {IPrepareApiParams}
-     */
-    private set signString(value);
+    setConnectionKeys(params: IConnectionKeysParams$1): void;
+    createInvoice(params: ICreateInvoiceParams): Promise<ICreateInvoiceResponse>;
+    checkPaymentStatusByMerchantTransId(params: ICheckPaymentStatusByMerchantTransId): Promise<ICheckPaymentStatusByMerchantTransIdResponse>;
+    checkInvoiceStatus(params: ICheckInvoiceStatusParams): Promise<ICheckInvoiceStatusResponse>;
+    checkPaymentStatus(params: ICheckPaymentStatusParams): Promise<ICheckPaymentStatusResponse>;
+    cancellationPayment(params: ICancellationPaymentParams): Promise<ICancellationPaymentResponse>;
+    refundPartial(params: IRefoundPartialParams): Promise<IRefoundPartialResponse>;
+    createTokenCard(params: ICreateTokenCardParams): Promise<ICreateTokenCardResponse>;
+    verificationTokenCard(params: IVerificationTokenCardParams): Promise<IVerificationTokenCardResponse>;
+    payWithToken(params: IPayWithTokenParams): Promise<IPayWithTokenResponse>;
+    deleteTokenCard(params: IDeleteTokenCardParams): Promise<IDeleteTokenCardResponse>;
     /**
      * Gets request for click service
      * @private
@@ -88,10 +183,82 @@ declare class ShoppingApi implements IShoppingApi {
     private validateCredentials;
 }
 
-type TPaymentApi = 'shopping_api' // other api
+interface ICompleteApiParams {
+    click_trans_id: number
+    service_id: number
+    click_paydoc_id: number
+    merchant_trans_id: string
+    amount: number
+    action: number
+    error: number
+    error_note: string
+    sign_time: string
+    sign_string: string
+    merchant_prepare_id: number
+}
+
+declare class ICompleteApiResponse {
+    click_trans_id: number
+    merchant_trans_id: string
+    merchant_confirm_id: number
+    error: number
+    error_note: string
+}
+
+interface IPrepareApiParams {
+    service_id: number
+    sign_time: string
+    sign_string: string
+    action: number
+    click_trans_id: number 
+    click_paydoc_id: number
+    amount: number
+    merchant_trans_id: string
+    error: number
+    error_note: string
+}
+
+interface IPrepareApiResponse {
+    click_trans_id: number
+    merchant_trans_id: string
+    merchant_prepare_id: number
+    error: number
+    error_note: string
+    sign_time: string
+    sign_string: string
+}
+
+interface IShoppingApi {
+    setConnectionKeys(params: IConnectionKeysParams$1): void
+    prepare(params: IPrepareApiParams): Promise<IPrepareApiResponse> 
+    complete(params: ICompleteApiParams): Promise<ICompleteApiResponse>
+}
+
+declare class ShoppingApi implements IShoppingApi {
+    private secret_key;
+    private merchant_id;
+    private service_id;
+    private user_id;
+    private sign_string;
+    setConnectionKeys(params: IConnectionKeysParams$1): void;
+    prepare(params: IPrepareApiParams): Promise<IPrepareApiResponse>;
+    complete(params: ICompleteApiParams): Promise<ICompleteApiResponse>;
+    private checkSignString;
+    private checkError;
+    /**
+     * Sets sign_string property
+     * @private
+     * @param {IPrepareApiParams}
+     */
+    private set signStringForPrepare(value);
+    private set signStringForComplete(value);
+    private validateCredentials;
+}
+
+type TPaymentApi = 'shopping' | 'merchant'
 
 declare class ClickClient {
-    static create(type: TPaymentApi): ShoppingApi;
+    static create<T extends TPaymentApi>(type: T): T extends 'merchant' ? MerchantApi : ShoppingApi;
 }
 
 export { ClickClient };
--- dist/index.mjs
+++ dist/index.mjs
@@ -19,97 +19,164 @@ var __async = (__this, __arguments, generator) => {
   });
 };
 
-// src/api-types/shopping-api.ts
+// src/constants/api-url.ts
+var CLICK_API_URL = "https://api.click.uz/v2/merchant/";
+var CREATED_INVOICE = CLICK_API_URL + "invoice/create";
+var CHECKED_STATUS_INVOICE = CLICK_API_URL + "invoice/status/";
+var CHECKED_STATUS_PAYMENT = CLICK_API_URL + "payment/status/";
+var CHECKED_STATUS_BY_MERCHANT_TRANS_ID = CLICK_API_URL + "payment/status_by_mti/";
+var REFUNDED_PARTIAL = CLICK_API_URL + "payment/partial_reversal/";
+var CANCELLED_PAYMENT = CLICK_API_URL + "payment/reversal/";
+var CREATED_TOKEN = CLICK_API_URL + "card_token/request";
+var VERIFICATED_TOKEN = CLICK_API_URL + "card_token/verify";
+var PAYED_BY_TOKEN = CLICK_API_URL + "card_token/payment";
+var DELETED_TOKEN = CLICK_API_URL + "card_token/";
+
+// src/api-types/merchant-api.ts
 import axios from "axios";
 import sha1 from "crypto-js/sha1";
-import md5 from "crypto-js/md5";
-
-// src/constants/api-url.ts
-var API_URL = "https://api.click.uz/v2/merchant/";
-var PREPARE_URL = API_URL + "/prepare";
-var COMPLETE_URL = API_URL + "/complete";
-
-// src/api-types/shopping-api.ts
-var ShoppingApi = class {
-  constructor() {
-    this.clickApiUrl = API_URL;
-    this.action = 0;
-    this.error = 0;
-    this.sign_time = (/* @__PURE__ */ new Date()).toString();
-  }
+var MerchantApi = class {
   setConnectionKeys(params) {
-    this.merchant_trans_id = params.merchant_trans_id;
-    this.service_id = params.service_id;
-    this.user_id = params.user_id;
+    this.merchant_id = params.merchant_id;
     this.secret_key = params.secret_key;
+    this.user_id = params.user_id;
+    this.service_id = params.service_id;
   }
-  prepare(params) {
+  createInvoice(params) {
     return __async(this, null, function* () {
       this.validateCredentials();
-      this.signString = params;
-      this.otherProperties = params;
       try {
-        const res = yield this.clickRequest.post(PREPARE_URL, {
-          click_trans_id: params.click_trans_id,
-          click_paydoc_id: params.click_paydoc_id,
+        const res = yield this.clickRequest.post(CREATED_INVOICE, {
+          service_id: params.service_id,
           amount: params.amount,
-          service_id: this.service_id,
-          merchant_trans_id: this.merchant_trans_id,
-          action: this.action,
-          error: this.error,
-          error_note: this.error_note,
-          sign_time: this.sign_time,
-          sign_string: this.sign_string
+          phone_number: params.phone_number,
+          merchant_trans_id: params.merchant_trans_id
         });
         return res.data;
       } catch (err) {
-        console.error(err);
-        throw new Error("Error while prepare");
+        throw new Error(`Error while create invoice "${err}"`);
       }
     });
   }
-  complete(params) {
+  checkPaymentStatusByMerchantTransId(params) {
     return __async(this, null, function* () {
+      this.validateCredentials();
       try {
-        const res = yield this.clickRequest.post(COMPLETE_URL, {
-          merchant_prepare_id: params.merchant_prepare_id,
-          click_trans_id: this.click_trans_id,
-          click_paydoc_id: this.click_paydoc_id,
-          amount: this.amount,
-          service_id: this.service_id,
-          merchant_trans_id: this.merchant_trans_id,
-          action: this.action,
-          error: this.error,
-          error_note: this.error_note,
-          sign_time: this.sign_time,
-          sign_string: this.sign_string
-        });
+        const res = yield this.clickRequest.get(
+          CHECKED_STATUS_BY_MERCHANT_TRANS_ID + params.service_id + "/" + params.merchant_trans_id + "/" + params.YYYY_MM_DD
+        );
         return res.data;
       } catch (err) {
-        console.error(err);
-        throw new Error("Error while prepare");
+        throw new Error(`Error while check payment status by merchant id "${err}"`);
       }
     });
   }
-  /**
-   * Sets amount, click_trans_id and click_paydoc_id properties
-   * @private
-   * @param {IPrepareApiParams} 
-   */
-  set otherProperties(params) {
-    this.amount = params.amount;
-    this.click_trans_id = params.click_trans_id;
-    this.click_paydoc_id = params.click_paydoc_id;
+  checkInvoiceStatus(params) {
+    return __async(this, null, function* () {
+      this.validateCredentials();
+      try {
+        const res = yield this.clickRequest.get(
+          CHECKED_STATUS_INVOICE + params.service_id + "/" + params.invoice_id
+        );
+        return res.data;
+      } catch (err) {
+        throw new Error(`Error while check invoice status "${err}"`);
+      }
+    });
   }
-  /**
-   * Sets sign_string property
-   * @private
-   * @param {IPrepareApiParams} 
-   */
-  set signString(params) {
-    const data = params.click_trans_id + this.service_id + this.secret_key + this.merchant_trans_id + params.amount + this.action + this.sign_time;
-    this.sign_string = sha1(data).toString();
+  checkPaymentStatus(params) {
+    return __async(this, null, function* () {
+      this.validateCredentials();
+      try {
+        const res = yield this.clickRequest.get(
+          CHECKED_STATUS_PAYMENT + params.service_id + "/" + params.payment_id
+        );
+        return res.data;
+      } catch (err) {
+        throw new Error(`Error while check status payment "${err}"`);
+      }
+    });
   }
+  cancellationPayment(params) {
+    return __async(this, null, function* () {
+      this.validateCredentials();
+      try {
+        const res = yield this.clickRequest.delete(CANCELLED_PAYMENT + "/" + params.service_id + "/" + params.payment_id);
+        return res.data;
+      } catch (err) {
+        throw new Error(`Error while cancellation payment "${err}"`);
+      }
+    });
+  }
+  refundPartial(params) {
+    return __async(this, null, function* () {
+      this.validateCredentials();
+      try {
+        const res = yield this.clickRequest.delete(REFUNDED_PARTIAL + "/" + params.service_id + "/" + params.payment_id + "/" + params.amount);
+        return res.data;
+      } catch (err) {
+        throw new Error(`Error while refundPartial "${err}"`);
+      }
+    });
+  }
+  createTokenCard(params) {
+    return __async(this, null, function* () {
+      this.validateCredentials();
+      try {
+        const res = yield this.clickRequest.post(CREATED_TOKEN, {
+          service_id: params.service_id,
+          card_number: params.card_number,
+          expire_date: params.expire_date,
+          temporary: params.temporary
+        });
+        return res.data;
+      } catch (err) {
+        throw new Error(`Error while createTokenCard "${err}"`);
+      }
+    });
+  }
+  verificationTokenCard(params) {
+    return __async(this, null, function* () {
+      this.validateCredentials();
+      try {
+        const res = yield this.clickRequest.post(VERIFICATED_TOKEN, {
+          service_id: params.service_id,
+          card_token: params.card_token,
+          sms_code: params.sms_code
+        });
+        return res.data;
+      } catch (err) {
+        throw new Error(`Error while verification token "${err}"`);
+      }
+    });
+  }
+  payWithToken(params) {
+    return __async(this, null, function* () {
+      this.validateCredentials();
+      try {
+        const res = yield this.clickRequest.post(PAYED_BY_TOKEN, {
+          card_token: params.card_token,
+          amount: params.amount,
+          merchant_trans_id: params.merchant_trans_id
+        });
+        return res.data;
+      } catch (err) {
+        throw new Error(`Error while pay with token payment "${err}"`);
+      }
+    });
+  }
+  deleteTokenCard(params) {
+    return __async(this, null, function* () {
+      this.validateCredentials();
+      try {
+        const res = yield this.clickRequest.delete(DELETED_TOKEN + params.service_id + "/" + params.card_token);
+        return res.data;
+      } catch (err) {
+        throw new Error(`Error while pay with token payment "${err}"`);
+      }
+    });
+  }
+  //make transfer merchant api
   /**
    * Gets request for click service
    * @private
@@ -117,7 +184,7 @@ var ShoppingApi = class {
    */
   get clickRequest() {
     return axios.create({
-      baseURL: this.clickApiUrl,
+      baseURL: CLICK_API_URL,
       headers: {
         "Accept": "application/json",
         "Content-Type": "application/json",
@@ -131,9 +198,8 @@ var ShoppingApi = class {
    * @private
    */
   get authorization() {
-    const timestampHash = md5(String(this.timestamp)).toString();
-    const secretKeyHash = md5(this.secret_key).toString();
-    return `${this.user_id}:${timestampHash}${secretKeyHash}:${this.timestamp}`;
+    const timestampHashAndSecretKeyHash = sha1(String(this.timestamp) + this.secret_key).toString();
+    return `${this.user_id}:${timestampHashAndSecretKeyHash}:${this.timestamp}`;
   }
   /**
    * Gets the current Unix timestamp in seconds.
@@ -147,18 +213,132 @@ var ShoppingApi = class {
     if (!this.secret_key) {
       throw new Error("secret key is required");
     }
-    if (!this.user_id) {
-      throw new Error("user_id is required");
+  }
+};
+
+// src/api-types/shopping-api.ts
+import md5 from "crypto-js/md5";
+
+// src/utils/error-exeptions/error.exception.ts
+var ErrorException = class {
+  constructor(error) {
+    this.checkError(error);
+  }
+  checkError(error) {
+    switch (error) {
+      case 0:
+        return;
+      case -1:
+        throw new Error("SIGN CHECK FAILED");
+      case -2:
+        throw new Error("Incorrect parameter amount");
+      case -3:
+        throw new Error("Action not found");
+      case -4:
+        throw new Error("Already paid");
+      case -5:
+        throw new Error("User does not exist");
+      case -6:
+        throw new Error("Transaction does not exist");
+      case -7:
+        throw new Error("Failed to update user");
+      case -8:
+        throw new Error("Error in request from click");
+      case -9:
+        throw new Error("Transaction cancelled");
     }
-    this.clickRequest;
+  }
+};
+
+// src/utils/generate-random-number.ts
+function getRandomNumber() {
+  return Math.floor(Math.random() * 1e4) + 1;
+}
+
+// src/api-types/shopping-api.ts
+var ShoppingApi = class {
+  setConnectionKeys(params) {
+    this.merchant_id = params.merchant_id;
+    this.secret_key = params.secret_key;
+    this.user_id = params.user_id;
+    this.service_id = params.service_id;
+  }
+  prepare(params) {
+    return __async(this, null, function* () {
+      this.validateCredentials();
+      this.signStringForPrepare = params;
+      this.checkSignString(params.sign_string);
+      this.checkError(params);
+      try {
+        return {
+          click_trans_id: params.click_trans_id,
+          merchant_trans_id: params.merchant_trans_id,
+          error: params.error,
+          error_note: params.error_note,
+          sign_string: params.sign_string,
+          sign_time: params.sign_time,
+          merchant_prepare_id: getRandomNumber()
+        };
+      } catch (err) {
+        console.error(err);
+        throw new Error("Error while prepare");
+      }
+    });
+  }
+  complete(params) {
+    return __async(this, null, function* () {
+      this.signStringForComplete = params;
+      this.checkError(params);
+      try {
+        return {
+          click_trans_id: params.click_trans_id,
+          merchant_trans_id: params.merchant_trans_id,
+          error: params.error,
+          error_note: params.error_note,
+          merchant_confirm_id: getRandomNumber()
+        };
+      } catch (err) {
+        console.error(err);
+        throw new Error("Error while prepare");
+      }
+    });
+  }
+  checkSignString(signString) {
+    if (signString !== this.sign_string) {
+      throw new Error("Sign string is not valid");
+    }
+  }
+  checkError(params) {
+    return new ErrorException(params.error);
+  }
+  /**
+   * Sets sign_string property
+   * @private
+   * @param {IPrepareApiParams} 
+   */
+  set signStringForPrepare(params) {
+    const data = params.click_trans_id + this.service_id + this.secret_key + params.merchant_trans_id + params.amount + params.action + params.sign_time;
+    this.sign_string = md5(data).toString();
+  }
+  set signStringForComplete(params) {
+    const data = params.click_trans_id + this.service_id + this.secret_key + params.merchant_trans_id + params.merchant_prepare_id + params.amount + params.action + params.sign_time;
+    this.sign_string = md5(data).toString();
+  }
+  validateCredentials() {
+    if (!this.secret_key) {
+      throw new Error("secret key is required");
+    }
   }
 };
 
 // src/index.ts
 var ClickClient = class {
   static create(type) {
-    if (type === "shopping_api") {
+    if (type === "shopping") {
       return new ShoppingApi();
+    }
+    if (type === "merchant") {
+      return new MerchantApi();
     } else {
       throw new Error("Invalid API type");
     }
